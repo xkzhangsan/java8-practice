8f431d07663e4a0d49e8b3ce1eaee0865c3b263c
xkzhangsan <xkzhangsan@163.com>
2019-05-11 14:00:34 +0800

    first commit

---
+++ src/main/java/com/xkzhangsan/normal/collectors/Person.java
@@ -0,0 +1,33 @@
+package com.xkzhangsan.normal.collectors;
+
+public class Person {
+	
+	private Integer id;
+	private String name;
+	private Integer score;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getScore() {
+		return score;
+	}
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+	
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", score=" + score + "]";
+	}
+	
+
+}
